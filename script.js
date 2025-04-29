93a017c6451f8ce8982009fce42d319703288238
barudaftare28 <lidyasantami@gmail.com>
2025-04-29 23:30:46 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+
+// Fade-in saat scroll
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".hero, .sinopsis, .pemeran, .dokumentasi");
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.style.animationPlayState = "running";
+          observer.unobserve(entry.target); // hanya animasi sekali
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+  
+    elements.forEach(el => {
+      el.style.animationPlayState = "paused";
+      observer.observe(el);
+    });
+  });
+  
